22de18a466563a8eeb08bb7f695df604b10268ad
facelessuser <faceless.shop@gmail.com>
2021-01-06 08:50:26 -0700

    Update build environment

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ generate_unicode_table()
 setup(
     name='backrefs',
     version=VER,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     keywords='regex re',
     description='A wrapper around re and regex that adds additional back references.',
     long_description=get_description(),
@@ -100,7 +100,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
