91faebd4ec09b75edc803949113d9f7dd009b966
Isaac Muse <faceless.shop@gmail.com>
2021-11-02 10:24:29 -0600

    Re format replace fixes (#168)
    * Re format replace fixes
    
    Re format replacement captures behave more like Regex in that you can
    technically index into the captures of a given group in Re, but in Re
    there is only ever one or zero captures. Documentation was never really
    explicit on what one should expect if indexing a group in Re occurred.
    The documentation seemed to vaguely insinuate that it would behave like
    a Regex capture list, just with one or zero values in the list. In
    reality the value was simple a string or `None`. This caused a bug in
    some cases where you'd have `None` inserted for a group if a group was
    optional, but referenced in the replacement template. Now the
    implementation matches the description in the documentation with the
    documentation now being more explicit about behavior.
    
    * Consolidate str/byte logic again and add missed test
    
    Better for maintenance as we just manage the one method typing concerns
    are resolved
    
    * Clean up docs a little more

--- tests/test_bre.py
+++ tests/test_bre.py
@@ -841,6 +841,11 @@ class TestSearchTemplate(unittest.TestCase):
 class TestReplaceTemplate(unittest.TestCase):
     """Test replace template."""
 
+    def test_existing_group_no_match(self):
+        """Test existing group with no match."""
+
+        self.assertEqual(bre.compile(r'(test)|(what)').sub(r'\2', 'test'), '')
+
     def test_hash(self):
         """Test hashing of replace."""
 
@@ -909,6 +914,11 @@ class TestReplaceTemplate(unittest.TestCase):
 
         with pytest.raises(TypeError):
             bre.subf(b'test', br'{[test]}', b'test', bre.FORMAT)
+
+    def test_format_ascii_align(self):
+        """Test format ASCII."""
+
+        self.assertEqual(bre.subf('test', r'{0!a:|^8}', 'test'), "|'test'|")
 
     def test_format_conversions(self):
         """Test string format conversion paths."""
@@ -1712,7 +1722,7 @@ class TestReplaceTemplate(unittest.TestCase):
         )
         results = expand(pattern.match(text))
         self.assertEqual(
-            'ba',
+            'ababababab',
             results
         )
 
@@ -1744,7 +1754,7 @@ class TestReplaceTemplate(unittest.TestCase):
         )
 
         results = expand(pattern.match(text))
-        self.assertEqual(b'989898', results)
+        self.assertEqual(b'bbb', results)
 
     def test_format_escapes(self):
         """Test format escapes."""
@@ -1970,16 +1980,10 @@ class TestReplaceTemplate(unittest.TestCase):
 class TestExceptions(unittest.TestCase):
     """Test Exceptions."""
 
-    def test_bad_group(self):
-        """Test bad group."""
-
-        with pytest.raises(TypeError):
-            bre.compile(r'(test)|(what)').sub(r'\2', 'test')
-
     def test_bad_format_group(self):
         """Test bad format group."""
 
-        with pytest.raises(TypeError):
+        with pytest.raises(IndexError):
             bre.compile(r'(test)|(what)').subf(r'{2}', 'test')
 
     def test_immutable(self):
