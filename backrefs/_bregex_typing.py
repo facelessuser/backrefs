12922bbcbae4f5fcc05a0779a53f4fd7a1f9bf0f
Isaac Muse <faceless.shop@gmail.com>
2025-02-25 08:36:26 -0700

    Drop Python 3.8 and start testing Python 3.14 (#189)
    * Drop Python 3.8 and start testing Python 3.14
    
    * Remove 3.8 specific code

--- backrefs/_bregex_typing.py
+++ backrefs/_bregex_typing.py
@@ -1,13 +1,6 @@
 """Typing objects for the Regex library."""
-from typing import AnyStr
 import regex  # type: ignore[import]
-import sys
-
 from typing import _alias  # type: ignore[attr-defined]
 
-if sys.version_info >= (3, 9):
-    Pattern = _alias(type(regex.compile('')), 1)
-    Match = _alias(type(regex.compile('').match('')), 1)
-else:
-    Pattern = _alias(type(regex.compile('')), AnyStr)
-    Match = _alias(type(regex.compile('').match('')), AnyStr)
+Pattern = _alias(type(regex.compile('')), 1)
+Match = _alias(type(regex.compile('').match('')), 1)
